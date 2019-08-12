26d3582d8fb51f61394d3f14a3dc94d6551e0125
v2000 <vrpr2008@gmail.com>
2019-08-12 13:58:09 +0200

    delete

--- includes/class-wdv-about-me-widget.php
+++ includes/class-wdv-about-me-widget.php
@@ -166,8 +166,6 @@ class Wdv_About_Me_Widget {
    $plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
    $this->loader->add_filter( 'plugin_action_links_' . $plugin_basename, $plugin_admin, 'add_action_links' );
 
-   // Save/Update our plugin options
-   $this->loader->add_action('admin_init', $plugin_admin, 'options_update');
     //Widgets
     $this->loader->add_action( 'widgets_init', $plugin_admin, 'register_widgets' );   
 	}
